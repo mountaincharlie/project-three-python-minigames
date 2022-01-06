4419f6333e6bf11bbab83b75c0992cbeb7bd3e53
Charlie <ciharland@hotmail.co.uk>
2022-01-06 18:08:07 +0000

    Add function to make a dict of unique usernames from the leaderboards sheets

--- leaderboards.py
+++ leaderboards.py
@@ -18,11 +18,33 @@ CLIENT = gspread.authorize(CREDS)
 SHEET = CLIENT.open("leaderboards")
 
 # checking it works
-first_leaderboard = SHEET.worksheet('minesweeper')
-data = first_leaderboard.get_all_values()
-pprint(data)
+# first_leaderboard = SHEET.worksheet('minesweeper')
+# data = first_leaderboard.get_all_values()
+# pprint(data)
 
 # accessing rows/columns/cells (delete)
-# row = first_leaderboard.row_value(2)
-# col = first_leaderboard.col_value(2)
+# row = first_leaderboard.row_values(2)
+# col = first_leaderboard.col_values(2)
 # cell = first_leaderboard.cell(2,2).value
+
+# DEFINE 'sheets' in run.py
+# sheets = ['minesweeper', 'hangman']
+
+# finding all unique usernames using a set and | operator
+def unique_usernames(sheets):
+    usernames = set()
+    # looping through each worksheet in the 'sheets' list
+    for sheet in sheets:
+        worksheet = SHEET.worksheet(sheet) # getting the worksheet
+        data = worksheet.col_values(2) # getting the value sin the username col
+        usernames |= set(data[1:]) # creating a union of both sets
+
+    # converting the set into dictionary weith number keys
+    usernames_dict = {}
+    for i, name in enumerate(usernames):
+        usernames_dict[i] = name
+    print(usernames_dict)
+
+
+# test calling functions
+# unique_usernames(sheets)
