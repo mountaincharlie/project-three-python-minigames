e82e9e6a7cf0d5c17dc67fc52d715426077f1cf3
Charlie <ciharland@hotmail.co.uk>
2022-01-08 17:38:45 +0000

    Install numpy and use it to generate a 2d array of zeroes

--- modules/minesweeper.py
+++ modules/minesweeper.py
@@ -1,6 +1,23 @@
 """ Minesweeper minigame module for Python Minigames """
 
+# imports
+import numpy as np
+
 
 # welcome message function
 def welcome_msg():
     print('Welcome to the Minesweeper minigame!')
+
+
+'''
+-create the 2d array hidden_grid
+-break into functions each process to build and setup the array
+(generate, add bombs, add numbers to other coors)
+'''
+# gerating a 2d array of zeroes from defined rows and cols
+rows = 9
+cols = 9
+hidden_grid = np.zeros((rows, cols), dtype=int)
+print(hidden_grid)
+
+# generate a number of random coordinates to insert bombs
