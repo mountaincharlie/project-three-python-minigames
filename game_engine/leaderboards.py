e3f9bc37c1b3f96a22b5df7ccccfdca106b67e87
Charlie <ciharland@hotmail.co.uk>
2022-01-12 11:12:18 +0000

    Create leaderboard_user instance of run.Player class and add leaderboard_choice function

--- game_engine/leaderboards.py
+++ game_engine/leaderboards.py
@@ -105,20 +105,52 @@ def rank_refresh(leaderboard):
         leaderboard.update_cell(row, 1, rank)
 
 
+# choosing a leaderboard option
+def leaderboard_choice(user, leaderboards_menu):
+    """
+    """
+    while True:
+        try:
+            # make a leaderboard print functions?
+            print('Leaderboards Menu:')
+            for key in leaderboards_menu:
+                game_name = leaderboards_menu[key][5:]
+                print(key, '-', game_name)
+
+            leaderboard = input("\nEnter a number to select an option (or 'quit' to exit):\n")
+
+            if leaderboard == 'quit':
+                user.quit_status = leaderboard
+                break
+            elif leaderboard in leaderboards_menu:
+                leaderboard_name = leaderboards_menu[leaderboard]
+
+                print(f'\nOpening {leaderboard_name} ... \n')
+
+                # open and display the sheet
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print("\nInvalid entry. Enter a number from the options below.\nOr 'quit' to exit\n")
+
+
 def main(user):
     """
     """
     # setting username and calling welcome message
     username = user.username
-    welcome_msg(username)
+    # creating new instance of Player (so quit status doesnt affect in run.py)
+    leaderboard_user = run.Player(username, 0, None, None)
+    welcome_msg(leaderboard_user.username)
 
+    # make function?
     leaderboards_menu = run.menu_dict
     to_pop = str(len(leaderboards_menu))
     leaderboards_menu.pop(to_pop)
 
-    print('Leaderboards Menu:')
-    for key in leaderboards_menu:
-        game_name = leaderboards_menu[key][5:]
-        print(key,'-', game_name)
+    # make leaderboard_choice function and call by
+    while leaderboard_user.quit_status != 'quit':
+        leaderboard_choice(leaderboard_user, leaderboards_menu)
 
     print(f'\nThank you {username} for using the Python Minigame Leaderboards!\n')
