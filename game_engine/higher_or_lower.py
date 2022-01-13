8010e73709485498722ba1e779f0092cdd2facc5
Charlie <ciharland@hotmail.co.uk>
2022-01-13 11:26:03 +0000

    Rename from hangman to higher_or_lower and create game specific subclass

--- game_engine/higher_or_lower.py
+++ game_engine/higher_or_lower.py
@@ -1,6 +1,21 @@
-""" Hangman minigame module for Python Minigames """
+""" Higher_or_Lower minigame module for Python Minigames """
+
+# imports
+import numpy as np
+# to import run.py from parent directory
+import sys
+sys.path.append('.')
+from run import Player
 
 
-# welcome message function
-def welcome_msg():
-    print('Welcome to the Hangman minigame!')
+# Higher_or_Lower Player subclass
+class Higher_or_LowerPlayer(Player):
+    """ Creates instance of Higher_or_LowerPlayer """
+    def __init__(self, username, score, user_quit, game_choice, coors):
+        super().__init__(username, score, user_quit, game_choice)
+        self.coors = coors
+
+    @classmethod
+    def from_current_user(cls, player_instance, coors):
+        return cls(**player_instance.__dict__, coors=coors)
+
