a0f76dc559afeb7849dfcde2f25b593348241113
Charlie <ciharland@hotmail.co.uk>
2022-01-14 12:59:13 +0000

    Add functions to generate a rand card from deck and validate user guess

--- game_engine/higher_or_lower.py
+++ game_engine/higher_or_lower.py
@@ -20,10 +20,43 @@ class Higher_or_LowerPlayer(Player):
         return cls(**player_instance.__dict__, guess=guess)
 
 
+# random card choosing function
+def random_card(deck):
+    """
+    """
+    # randomly chooses and removes card from deck
+    card = np.random.choice(deck)
+    deck.remove(card)
+
+    return card
+
+
+# ----- PROCESSING USER INPUT -----
+
+def validate_guess():
+    """
+    Keeps asking the user for a guess of h (higher),
+    or l (lower) until a valid entry is given.
+    Returns the valid guess.
+    """
+
+    options = ['h', 'l']
+    while True:
+        try:
+            guess = input("Enter 'h' (for higher) 'l' (for lower):\n")
+            if guess not in options:
+                raise ValueError
+            else:
+                break
+        except ValueError:
+            print(f"Invalid guess")
+
+    return guess
+
+
 # main function call
 def main(user):
     """ main game function calls """
-    print('hello')
 
     # instance of Higher_or_LowerPlayer SubClass (no current guess value)
     higher_or_lower_user = Higher_or_LowerPlayer.from_current_user(user, None)
@@ -40,23 +73,33 @@ def main(user):
             break
         print('\nBuilding the ... ...\n')
 
+        # ----- SETTING UP THE GAME -----
+
         # build the card_order tuple [make a constant?]
         card_order = ('A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2')
         # build the deck list [make a function call so its reset at game start but not during game]
         deck = ['A_spades', 'K_spades', 'Q_spades', 'J_spades', '10_spades', '9_spades', '8_spades', '7_spades', '6_spades', '5_spades', '4_spades', '3_spades', '2_spades', 'A_diamonds', 'K_diamonds', 'Q_diamonds', 'J_diamonds', '10_diamonds', '9_diamonds', '8_diamonds', '7_diamonds', '6_diamonds', '5_diamonds', '4_diamonds', '3_diamonds', '2_diamonds', 'A_clubs', 'K_clubs', 'Q_clubs', 'J_clubs', '10_clubs', '9_clubs', '8_clubs', '7_clubs', '6_clubs', '5_clubs', '4_clubs', '3_clubs', '2_clubs', 'A_hearts', 'K_hearts', 'Q_hearts', 'J_hearts', '10_hearts', '9_hearts', '8_hearts', '7_hearts', '6_hearts', '5_hearts', '4_hearts', '3_hearts', '2_hearts']
-        cards_shown = []
+        cards_shown = []  # CHECK at end if we need this
 
         # printing the basic instructions (just a print)
         print(f'The first card from the deck is displayed below!\n\nChoose whether you think the next card to be revealed\nwill be higher or lower than this card.\nThe cards in order from highest to lowest are:\n{card_order}\n')
 
         # print initial random card [random card choosing function]
+        initial_card = random_card(deck)
+        print(initial_card)
+        # adds the initial_card to the cards_shown list
+        cards_shown.append(initial_card)
+        # print(deck)
+        # print(cards_shown)
 
         # while loop for prompting user guesses until they choose to quit
-        # while True:
+        while True:
 
-        # takes in and validates user guess [in a function]
+            # takes in and validates user guess [in a function]
+            user_guess = validate_guess()
+            print('user guess: ', user_guess)
 
-        # updates higher_or_lower_user.guess
+            # updates higher_or_lower_user.guess
 
         # prints their guess??
         # print(f"\nYou have guessed that the next card will be: {higher_or_lower_user.guess} than {current_card}\n")
