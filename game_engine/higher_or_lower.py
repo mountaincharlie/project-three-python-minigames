319f40a7fcb38c16143275a20351ba464d5bdd69
Charlie <ciharland@hotmail.co.uk>
2022-01-14 23:01:14 +0000

    Move code for when the game is finished to game_finished function in Player class in run.py

--- game_engine/higher_or_lower.py
+++ game_engine/higher_or_lower.py
@@ -11,8 +11,8 @@ from run import Player
 # Higher_or_Lower Player subclass
 class Higher_or_LowerPlayer(Player):
     """ Creates instance of Higher_or_LowerPlayer """
-    def __init__(self, username, score, user_quit, game_choice, guess):
-        super().__init__(username, score, user_quit, game_choice)
+    def __init__(self, username, score, user_quit, game_choice, score_order, guess):
+        super().__init__(username, score, user_quit, game_choice, score_order)
         self.guess = guess
 
     @classmethod
@@ -127,8 +127,8 @@ def main(user):
         # ----- SETTING UP THE GAME -----
 
         # build the deck list [make a function call so its reset at game start but not during game]
-        #deck = ['A_spades', 'K_spades', 'Q_spades', 'J_spades', '10_spades', '9_spades', '8_spades', '7_spades', '6_spades', '5_spades', '4_spades', '3_spades', '2_spades', 'A_diamonds', 'K_diamonds', 'Q_diamonds', 'J_diamonds', '10_diamonds', '9_diamonds', '8_diamonds', '7_diamonds', '6_diamonds', '5_diamonds', '4_diamonds', '3_diamonds', '2_diamonds', 'A_clubs', 'K_clubs', 'Q_clubs', 'J_clubs', '10_clubs', '9_clubs', '8_clubs', '7_clubs', '6_clubs', '5_clubs', '4_clubs', '3_clubs', '2_clubs', 'A_hearts', 'K_hearts', 'Q_hearts', 'J_hearts', '10_hearts', '9_hearts', '8_hearts', '7_hearts', '6_hearts', '5_hearts', '4_hearts', '3_hearts', '2_hearts']
-        deck = ['A_spades', 'A_diamonds', 'A_clubs', 'A_hearts']
+        deck = ['A_spades', 'K_spades', 'Q_spades', 'J_spades', '10_spades', '9_spades', '8_spades', '7_spades', '6_spades', '5_spades', '4_spades', '3_spades', '2_spades', 'A_diamonds', 'K_diamonds', 'Q_diamonds', 'J_diamonds', '10_diamonds', '9_diamonds', '8_diamonds', '7_diamonds', '6_diamonds', '5_diamonds', '4_diamonds', '3_diamonds', '2_diamonds', 'A_clubs', 'K_clubs', 'Q_clubs', 'J_clubs', '10_clubs', '9_clubs', '8_clubs', '7_clubs', '6_clubs', '5_clubs', '4_clubs', '3_clubs', '2_clubs', 'A_hearts', 'K_hearts', 'Q_hearts', 'J_hearts', '10_hearts', '9_hearts', '8_hearts', '7_hearts', '6_hearts', '5_hearts', '4_hearts', '3_hearts', '2_hearts']
+        # deck = ['A_spades', 'A_diamonds', 'A_clubs', 'A_hearts']
         cards_shown = []  # CHECK at end if we need this
 
         # printing the basic instructions (just a print)
@@ -172,44 +172,33 @@ def main(user):
             print(higher_or_lower_user.score)
 
             if guess_check == 'wrong':
+                print(f'\nSorry {higher_or_lower_user.username}, the {new_card} is not {user_guess} than the {revealed_card}.\n')
+                higher_or_lower_user.game_finish(cards_shown)
                 # make game_finished() function
-                print(f'\nSorry {higher_or_lower_user.username}, the {new_card} is not {user_guess} than the {revealed_card}.\nYour correct guess streak was: {higher_or_lower_user.score}')
-                if len(cards_shown) != 0:
-                    print(f'You made correct guesses for the following cards:\n{cards_shown}')
-
-                # prompts play again or quit (in self.play_again())
-                choice = input("Hit ENTER to play again or 'quit' to return to the Main Menu:\n")
-                if choice == 'quit':
-                    higher_or_lower_user.quit_status = 'quit'
-                break
             else:
                 # updating number of shown cards if correctly guessed
                 cards_shown.append(new_card)
+                # prompts guess again or quit
+                cont_or_quit = input("\nHit ENTER to guess again or 'quit' to restart the game:\n")
+                if cont_or_quit.lower() == 'quit':
+                    break
 
             if len(deck) == 0:
-                # add to game_finished() function
-                print(f'\nCongratulations {higher_or_lower_user.username},\nYour correct guess streak was: {higher_or_lower_user.score}')
-                print(f'You made correct guesses for the following cards:\n{cards_shown}')
-                # trigger leaderboard update
-
-                # prompts play again or quit (in self.play_again())
-                choice = input("Hit ENTER to play again or 'quit' to return to the Main Menu:\n")
-                if choice == 'quit':
-                    higher_or_lower_user.quit_status = 'quit'
-                break
+                print(f'\nCongratulations {higher_or_lower_user.username}!\n')
+                higher_or_lower_user.game_finish(cards_shown)
 
             # updates the user's score [in a function]
             # prompts game_win or game_lost function
 
             # prompts guess again or quit
-            cont_or_quit = input("\nHit ENTER to guess again or 'quit' to restart the game:\n")
-            if cont_or_quit.lower() == 'quit':
-                break
+            # cont_or_quit = input("\nHit ENTER to guess again or 'quit' to restart the game:\n")
+            # if cont_or_quit.lower() == 'quit':
+            #    break
 
 
 # game constants
-#CARD_ORDER = ('A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2')
-CARD_ORDER = ('A')
+CARD_ORDER = ('A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2')
+# CARD_ORDER = ('A')
 
 # calling main()
 # main()
