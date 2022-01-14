e01f39fc5556ff84b0fdc63aef6817b0dc32fd67
Charlie <ciharland@hotmail.co.uk>
2022-01-14 12:33:07 +0000

    Create card_order tuple, deck list and initial instructions to user

--- game_engine/higher_or_lower.py
+++ game_engine/higher_or_lower.py
@@ -40,9 +40,14 @@ def main(user):
             break
         print('\nBuilding the ... ...\n')
 
-        # build the game lists/tuples [function call]
+        # build the card_order tuple [make a constant?]
+        card_order = ('A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2')
+        # build the deck list [make a function call so its reset at game start but not during game]
+        deck = ['A_spades', 'K_spades', 'Q_spades', 'J_spades', '10_spades', '9_spades', '8_spades', '7_spades', '6_spades', '5_spades', '4_spades', '3_spades', '2_spades', 'A_diamonds', 'K_diamonds', 'Q_diamonds', 'J_diamonds', '10_diamonds', '9_diamonds', '8_diamonds', '7_diamonds', '6_diamonds', '5_diamonds', '4_diamonds', '3_diamonds', '2_diamonds', 'A_clubs', 'K_clubs', 'Q_clubs', 'J_clubs', '10_clubs', '9_clubs', '8_clubs', '7_clubs', '6_clubs', '5_clubs', '4_clubs', '3_clubs', '2_clubs', 'A_hearts', 'K_hearts', 'Q_hearts', 'J_hearts', '10_hearts', '9_hearts', '8_hearts', '7_hearts', '6_hearts', '5_hearts', '4_hearts', '3_hearts', '2_hearts']
+        cards_shown = []
 
         # printing the basic instructions (just a print)
+        print(f'The first card from the deck is displayed below!\n\nChoose whether you think the next card to be revealed\nwill be higher or lower than this card.\nThe cards in order from highest to lowest are:\n{card_order}\n')
 
         # print initial random card [random card choosing function]
 
